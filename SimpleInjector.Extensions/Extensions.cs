009223e2d387fe6f968156943afd5cfdb1518b77
Toni Rossmann <toni@rti.srv.br>
2017-08-10 12:51:24 -0300

    Added optional parameters to filter by namespace

--- SimpleInjector.Extensions/Extensions.cs
+++ SimpleInjector.Extensions/Extensions.cs
@@ -6,11 +6,14 @@ namespace RTI.SimpleInjector.Extensions
 {
     public static class Extensions
     {
-        public static void RegisterImplementationsFromAssembly(this Container container, Assembly assembly, Lifestyle lifestyle)
+        public static void RegisterImplementationsFromAssembly(this Container container, Assembly assembly, Lifestyle lifestyle, params string[] namespaceFilter)
         {
             var types = assembly
                 .GetTypes()
                 .Where(type => type.GetCustomAttributes(typeof(DependencyInjectionAttribute), true).Length > 0);
+
+            if (namespaceFilter.Length > 0)
+                types = types.Where(t => namespaceFilter.Any(n => t.Namespace.StartsWith(n)));
 
             foreach (var implementationType in types)
             {
